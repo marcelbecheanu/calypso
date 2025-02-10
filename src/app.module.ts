2cc932c02cb3e529d0a6d91afb808fb7a96cf026
Marcel Becheanu <marcel.becheanu@outlook.pt>
2025-02-10 02:38:29 +0000

    Chore: Adding typeorm.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { Identity } from './identity/identity.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [Identity],
+  imports: [
+    TypeOrmModule.forRoot({
+      synchronize: true,
+      type: 'postgres',
+      host: "127.0.0.1",
+      port: 5432,
+      username: "calypso",
+      password: "calypso",
+      database: "calypso",
+      entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+    }),
+    Identity
+  ],
   controllers: [],
   providers: [],
 })
--- src/identity/entities/identity.entity.ts
+++ src/identity/entities/identity.entity.ts
@@ -1,3 +1,54 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity({ name: `identity` })
 export class Identity {
+  @PrimaryGeneratedColumn({ name: 'identity_id' })
   id: number;
+
+  @Column({
+    name: 'identity_email',
+    unique: true,
+    nullable: false,
+    length: 320,
+  })
+  email: string;
+
+  @Column({
+    name: 'identity_password',
+    nullable: false,
+    length: 128,
+  })
+  password: string;
+
+  @Column({
+    name: 'is_confirmed',
+    default: false,
+  })
+  isConfirmed: boolean;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamp',
+    nullable: true,
+  })
+  deletedAt: Date;
 }
